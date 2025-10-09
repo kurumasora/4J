0683481a817336845564c0afe16290e26b647186
ahirukuma <slugsalt@icloud.com>
2025-10-09 18:07:06 +0900

    線形リストの実装途中まで。sarch関数が激ムズ

--- 3/3-3/LinkedList.h
+++ 3/3-3/LinkedList.h
@@ -2,9 +2,8 @@
 #define __LinkedList
 
 typedef struct __node{
-    int data;//データそのもの
-    struct __node *next;//次のノードのポインタ(次のノードの構造体全体にアクセスしたい)
-    //↑自己参照構造体：https://daeudaeu.com/c-self-relation/
+    int data;
+    struct __node *next;
 } Node;
 
 typedef struct {
@@ -15,13 +14,13 @@ typedef struct {
 
 
 void initialize(List *l);
-void insertFront(List *l, const int value);//1
-void insertRear(List *l, const int value);//2
+void insertFront(List *l, const int x);//1
+void insertRear(List *l, const int x);//2
 void removeFront(List *l);//3
 void removeRear(List *l);//4
-Node *search(List *l, const int value, int *n, Node **pre);
-int removeSearch(List *l, const int value);//5
-int searchIndex(List *l, const int value);//6
+Node *search(List *l, const int x, int *n, Node **pre);
+int removeSearch(List *l, const int x);//5
+int searchIndex(List *l, const int x);//6
 int size(List *l);//7
 void clear(List *l);//
 void print(const List *l);
